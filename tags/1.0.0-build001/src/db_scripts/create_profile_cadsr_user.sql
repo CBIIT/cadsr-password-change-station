614b8de8a05d6beb1476309fd607ac5100d34e33
Greg Raley <raleygj@mail.nih.gov>
2012-06-08 21:04:30 +0000

    Fix for password re-use never allowed
    SVN-Revision: 28

--- tags/1.0.0-build001/src/db_scripts/create_profile_cadsr_user.sql
+++ tags/1.0.0-build001/src/db_scripts/create_profile_cadsr_user.sql
@@ -4,7 +4,7 @@ create profile "cadsr_user" limit
  password_life_time 60
  password_grace_time 0
  password_reuse_max 24
- password_reuse_time UNLIMITED
+ password_reuse_time 1
  failed_login_attempts 6
  password_lock_time 60/1440
  password_verify_function password_verify_casdr_user
