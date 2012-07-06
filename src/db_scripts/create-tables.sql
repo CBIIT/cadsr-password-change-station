89052c982ecf75b00f013dcf2e187b1aab1e9cb1
James Tan <tanj3@mail.nih.gov>
2012-07-06 21:14:52 +0000

    Modified fields and table name so that they are consistent with others.
    SVN-Revision: 37

--- src/db_scripts/create-tables.sql
+++ src/db_scripts/create-tables.sql
@@ -1,18 +1,18 @@
-CREATE TABLE securityQnA (
-	username varchar2(30) NOT NULL,
+CREATE TABLE User_Security_Questions (
+	ua_name varchar2(30) NOT NULL,
 	question1 varchar2(500) NOT NULL,
 	answer1 varchar2(500) NOT NULL,
 	question2 varchar2(500) NOT NULL,
 	answer2 varchar2(500) NOT NULL,
 	question3 varchar2(500) NOT NULL,
 	answer3 varchar2(500) NOT NULL,
-	updated_date date NOT NULL
+	date_modified date NOT NULL
 );
 
-ALTER TABLE securityQnA ADD CONSTRAINT pk_securityQnA PRIMARY KEY (
-	username
+ALTER TABLE User_Security_Questions ADD CONSTRAINT pk_User_Security_Questions PRIMARY KEY (
+	ua_name
 );
 
-CREATE UNIQUE INDEX inx_qna ON securityQnA (
-	username
+CREATE UNIQUE INDEX inx_qna ON User_Security_Questions (
+	ua_name
 );
