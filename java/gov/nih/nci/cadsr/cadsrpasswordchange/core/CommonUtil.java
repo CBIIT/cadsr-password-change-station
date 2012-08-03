aff862e0841ef72c36cfd07e33f23d05c6d02f63
James Tan <tanj3@mail.nih.gov>
2012-08-03 19:16:27 +0000

    Support encryption of security answers.
    SVN-Revision: 121

--- java/gov/nih/nci/cadsr/cadsrpasswordchange/core/CommonUtil.java
+++ java/gov/nih/nci/cadsr/cadsrpasswordchange/core/CommonUtil.java
@@ -2,9 +2,52 @@ package gov.nih.nci.cadsr.cadsrpasswordchange.core;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.security.GeneralSecurityException;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 public class CommonUtil {
 
+	private static OracleObfuscation x;
+	static {
+		try {
+			x = new OracleObfuscation("$_12345&");
+		} catch (GeneralSecurityException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Pad the String up to the specified length passed.
+	 * @param text
+	 * @param length
+	 * @return
+	 */
+	public static String pad(String text, int length) {
+		String retVal = text;
+
+		if(text != null) {
+			retVal = String.format("%"+ (length - 4)+"s", text).replace(' ', '*');	//final string must be a multiples of 8 bytes
+		}
+		
+		return retVal;
+	}
+		
+	public static String encode(String text) throws GeneralSecurityException {
+		if(text != null) {
+			text = new String(Hex.encodeHex(x.encrypt(CommonUtil.pad(text, DAO.MAX_ANSWER_LENGTH).getBytes())));
+		}
+    	return text;
+    }
+
+	public static String decode(String text) throws GeneralSecurityException, DecoderException {
+		if(text != null) {
+			text = new String(x.decrypt(Hex.decodeHex(text.toCharArray())));
+		}
+    	return text;
+    }
+	
 	public static String toString(Throwable theException) {
 		// Create a StringWriter and a PrintWriter both of these object
 		// will be used to convert the data in the stack trace to a string.
