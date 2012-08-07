455db8fc7e321af53624837559ecc077ee645544
James Tan <tanj3@mail.nih.gov>
2012-08-07 16:12:32 +0000

    SVN-Revision: 127

---
+++ java/gov/nih/nci/cadsr/cadsrpasswordchange/core/PropertyHelper.java
@@ -0,0 +1,23 @@
+package gov.nih.nci.cadsr.cadsrpasswordchange.core;
+
+import java.util.Map;
+
+public class PropertyHelper {
+
+	public static String HELP_LINK;
+	public static String LOGO_LINK;
+	
+	public static String getHELP_LINK() {
+		return HELP_LINK;
+	}
+	public static void setHELP_LINK(String hELP_LINK) {
+		HELP_LINK = hELP_LINK;
+	}
+	public static String getLOGO_LINK() {
+		return LOGO_LINK;
+	}
+	public static void setLOGO_LINK(String lOGO_LINK) {
+		LOGO_LINK = lOGO_LINK;
+	}
+	
+}
