5ce781da451ec21b62d8c995ff895ab00eeedf07
James Tan <tanj3@mail.nih.gov>
2012-08-07 19:02:17 +0000

    SVN-Revision: 137

--- src/db_scripts/insert-web-links.sql
+++ src/db_scripts/insert-web-links.sql
@@ -1,3 +1,5 @@
+--run as sbrext
+
 insert into sbrext.tool_options_view_ext (Tool_name, Property, Value) values('PasswordChangeStation', 'HELP.ROOT', 'https://wiki.nci.nih.gov/x/vADgB')
 /
 insert into sbrext.tool_options_view_ext (Tool_name, Property, Value) values('PasswordChangeStation', 'LOGO.ROOT', 'https://cabig.nci.nih.gov')
