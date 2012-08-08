7f0a0fa85913dd8e046b391a0f19e78ed3fba67f
James Tan <tanj3@mail.nih.gov>
2012-08-08 20:18:52 +0000

    SVN-Revision: 145

--- java/gov/nih/nci/cadsr/cadsrpasswordchange/core/EmailSending.java
+++ java/gov/nih/nci/cadsr/cadsrpasswordchange/core/EmailSending.java
@@ -1,13 +1,13 @@
 package gov.nih.nci.cadsr.cadsrpasswordchange.core;
 
 import java.util.Properties;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+//import javax.mail.Authenticator;
+//import javax.mail.Message;
+//import javax.mail.PasswordAuthentication;
+//import javax.mail.Session;
+//import javax.mail.Transport;
+//import javax.mail.internet.InternetAddress;
+//import javax.mail.internet.MimeMessage;
 
 public class EmailSending {
 //	
