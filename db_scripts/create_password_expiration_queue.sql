0d16796e041c94e3b32dd318f70bf512c93b7892
James Tan <tanj3@mail.nih.gov>
2012-08-14 20:13:27 +0000

    SVN-Revision: 167

--- db_scripts/create_password_expiration_queue.sql
+++ db_scripts/create_password_expiration_queue.sql
@@ -2,10 +2,9 @@
 
 CREATE TABLE PASSWORD_NOTIFICATION (
 	ua_name varchar2(30) NOT NULL,
-	email_address varchar2(255) NOT NULL,
-	email_subject varchar2(255) NOT NULL,
-	email_body varchar2(2000) NOT NULL,
 	date_modified date NOT NULL,
 	attempted_count number(19),
+	processing_type varchar2(30),
 	delivery_status varchar2(30) NOT NULL
+	CONSTRAINT pk PRIMARY KEY (ua_name)
 );
