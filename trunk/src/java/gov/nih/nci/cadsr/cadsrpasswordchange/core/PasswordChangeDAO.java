2f455a0224f23c009817c6f213ee023727735981
James Tan <tanj3@mail.nih.gov>
2012-08-20 17:01:40 +0000

    Started refactoring of the codes.
    SVN-Revision: 186

--- trunk/src/java/gov/nih/nci/cadsr/cadsrpasswordchange/core/PasswordChangeDAO.java
+++ trunk/src/java/gov/nih/nci/cadsr/cadsrpasswordchange/core/PasswordChangeDAO.java
@@ -1,5 +1,7 @@
 package gov.nih.nci.cadsr.cadsrpasswordchange.core;
 
+import gov.nih.nci.cadsr.cadsrpasswordchange.domain.UserSecurityQuestion;
+
 import java.security.GeneralSecurityException;
 import java.sql.Connection;
 import java.sql.Date;
@@ -15,7 +17,7 @@ import javax.sql.DataSource;
 import org.apache.log4j.Logger;
 import org.joda.time.DateTimeUtils;
 
-public class DAO implements AbstractDao {
+public class PasswordChangeDAO implements PasswordChange {
 
     public static String _jndiUser = "java:/jdbc/caDSR";
     public static String _jndiSystem = "java:/jdbc/caDSRPasswordChange";
@@ -29,13 +31,13 @@ public class DAO implements AbstractDao {
 
     private static final String SQL_INSERT = "INSERT INTO " + QUESTION_TABLE_NAME + " (ua_name,question1,answer1,question2,answer2,question3,answer3,date_modified) VALUES (?,?,?,?,?,?,?,?)";
 
-    private Logger logger = Logger.getLogger(DAO.class);
+    private Logger logger = Logger.getLogger(PasswordChangeDAO.class);
 
-    public DAO(DataSource datasource) {
+    public PasswordChangeDAO(DataSource datasource) {
     	this.datasource = datasource;
     }
 
-    public DAO(Connection conn) {
+    public PasswordChangeDAO(Connection conn) {
     	this.conn = conn;
     }
     
@@ -52,7 +54,7 @@ public class DAO implements AbstractDao {
 		ResultSet rs = null;
 		try {
 	        if(conn == null) {
-			DataSource ds = ConnectionUtil.getDS(DAO._jndiSystem);
+			DataSource ds = ConnectionUtil.getDS(PasswordChangeDAO._jndiSystem);
 	        logger.debug("got DataSource for " + _jndiSystem);
 	        conn = ds.getConnection();
 	        }
@@ -129,7 +131,7 @@ public class DAO implements AbstractDao {
 
 		try {
 	        if(conn == null) {
-				DataSource ds = ConnectionUtil.getDS(DAO._jndiSystem);
+				DataSource ds = ConnectionUtil.getDS(PasswordChangeDAO._jndiSystem);
 		        logger.debug("got DataSource for " + _jndiSystem);
 	        	
 		        conn = ds.getConnection();
@@ -186,7 +188,7 @@ public class DAO implements AbstractDao {
             logger.debug("findByPrimaryKey sql : " + sql);
 
 	        if(conn == null) {
-            DataSource ds = ConnectionUtil.getDS(DAO._jndiSystem);
+            DataSource ds = ConnectionUtil.getDS(PasswordChangeDAO._jndiSystem);
 	        logger.debug("got DataSource for " + _jndiSystem);
         	
 	        conn = ds.getConnection();
@@ -233,7 +235,7 @@ public class DAO implements AbstractDao {
             sql = "select * from " + QUESTION_TABLE_NAME;
 
 	        if(conn == null) {
-            DataSource ds = ConnectionUtil.getDS(DAO._jndiSystem);
+            DataSource ds = ConnectionUtil.getDS(PasswordChangeDAO._jndiSystem);
 	        logger.debug("got DataSource for " + _jndiSystem);
         	
 	        conn = ds.getConnection();
@@ -285,7 +287,7 @@ public class DAO implements AbstractDao {
 
         try {
 	        if(conn == null) {
-        	DataSource ds = ConnectionUtil.getDS(DAO._jndiSystem);
+        	DataSource ds = ConnectionUtil.getDS(PasswordChangeDAO._jndiSystem);
 	        logger.debug("got DataSource for " + _jndiSystem);
         	
 	        conn = ds.getConnection();
@@ -447,7 +449,7 @@ public class DAO implements AbstractDao {
         try {
             if (params != null && params.length > 0) {
     	        if(conn == null) {
-            	DataSource ds = ConnectionUtil.getDS(DAO._jndiSystem);
+            	DataSource ds = ConnectionUtil.getDS(PasswordChangeDAO._jndiSystem);
     	        logger.debug("got DataSource for " + _jndiSystem);
             	
     	        conn = ds.getConnection();
@@ -526,7 +528,7 @@ public class DAO implements AbstractDao {
 		
 		try {
 	        if(conn == null) {				
-	        	DataSource ds = ConnectionUtil.getDS(DAO._jndiSystem);
+	        	DataSource ds = ConnectionUtil.getDS(PasswordChangeDAO._jndiSystem);
 		        logger.debug("got DataSource for " + _jndiSystem);
 	        	
 		        conn = ds.getConnection();
@@ -573,7 +575,7 @@ public class DAO implements AbstractDao {
 		
 		try {
 	        if(conn == null) {				
-	        	DataSource ds = ConnectionUtil.getDS(DAO._jndiSystem);
+	        	DataSource ds = ConnectionUtil.getDS(PasswordChangeDAO._jndiSystem);
 		        logger.debug("got DataSource for " + _jndiSystem);
 	        	
 		        conn = ds.getConnection();
@@ -598,245 +600,6 @@ public class DAO implements AbstractDao {
 		}
 
        return value;
-	}
-
-	/**
-	 * Method to retrieve all the user's with password which are expiring within the days specified in the passed in number.
-	 * @param withinDays
-	 * @return
-	 */
-	public List<User> getPasswordExpiringList(int withinDays) {
-
-		logger.info("getPasswordExpiringList entered");
-		
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		String value = null;
-		List arr = new ArrayList();
-		try {
-	        if(conn == null) {				
-	        	DataSource ds = ConnectionUtil.getDS(DAO._jndiSystem);
-		        logger.debug("got DataSource for " + _jndiSystem);
-	        	
-		        conn = ds.getConnection();
-	        }
-	        logger.debug("connected");
-
-			stmt = conn.prepareStatement(
-				"select " +
-				"   electronic_mail_address," +
-				"   PTIME," +
-				"   PROFILE," +
-				"   USERNAME," +
-				"   USER_ID," +
-				"   PASSWORD," +
-				"   ACCOUNT_STATUS," +
-				"   LOCK_DATE," +
-				"   EXPIRY_DATE," +
-				"   DEFAULT_TABLESPACE," +
-				"   TEMPORARY_TABLESPACE," +
-				"   CREATED," +
-				"   INITIAL_RSRC_CONSUMER_GROUP," +
-				"   EXTERNAL_NAME" +
-				" " +
-				"from sys.CADSR_USERS a, sbr.user_accounts_view b WHERE a.username = b.ua_name" +
-				" " +
-				"and EXPIRY_DATE BETWEEN SYSDATE AND SYSDATE+?"
-			);
-			stmt.setInt(1, withinDays);
-			rs = stmt.executeQuery();
-			if(rs.next()) {
-				User user = new User();
-				user.setElectronicMailAddress(rs.getString("electronic_mail_address"));
-				user.setUsername(rs.getString("username"));
-				user.setAccountStatus(rs.getString("account_status"));
-				user.setExpiryDate(rs.getDate("expiry_date"));
-				user.setLockDate(rs.getDate("lock_date"));
-				user.setPasswordChangedDate(rs.getDate("ptime"));
-				logger.info ("getRecipientList: mail_address '" + user.getElectronicMailAddress() + "', username '" + user.getUsername() + "' expiry_date '" + user.getExpiryDate() + "'");
-				arr.add(user);
-			}
-		} catch (Exception ex) {
-			logger.debug(ex.getMessage());
-		} finally {
-            if (rs != null) { try { rs.close(); } catch (SQLException e) { logger.error(e.getMessage()); } }
-            if (stmt != null) {  try { stmt.close(); } catch (SQLException e) { logger.error(e.getMessage()); } }
-        	if (conn != null) { try { conn.close(); conn = null; } catch (SQLException e) { logger.error(e.getMessage()); } }
-		}
-
-       return arr;
-	}
-
-	/**
-	 * Method to populate queue information specific to a user for password expiring notification.
-	 * @param user
-	 * @return populated user if found; otherwise null is returned
-	 * @throws Exception
-	 */
-	public User loadQueue(User user) throws Exception {
-
-		logger.info("getQueue entered");
-		if(user == null || user.getUsername() == null) {
-			throw new Exception("User or user's name can not be NULL or empty.");
-		}
-		
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		String value = null;
-		List arr = new ArrayList();
-		User ret = null;
-		try {
-	        if(conn == null) {				
-	        	DataSource ds = ConnectionUtil.getDS(DAO._jndiSystem);
-		        logger.debug("got DataSource for " + _jndiSystem);
-	        	
-		        conn = ds.getConnection();
-	        }
-	        logger.debug("connected");
-
-			stmt = conn.prepareStatement(
-					"select" +
-					"   c.electronic_mail_address," +
-					"   a.USERNAME," +
-					"   a.ACCOUNT_STATUS," +
-					"   a.EXPIRY_DATE," +
-					"   a.LOCK_DATE," +
-					"   a.PTIME, " +
-					"   b.DATE_MODIFIED," +
-					"   b.ATTEMPTED_COUNT," +
-					"   b.PROCESSING_TYPE," +
-					"   b.DELIVERY_STATUS" +
-					" from" +
-					" SYS.CADSR_USERS a, SBREXT.PASSWORD_NOTIFICATION b, sbr.user_accounts_view c" +
-					" where a.username = b.UA_NAME(+) and a.username = c.UA_NAME and a.username = ?"
-			);
-			stmt.setString(1, user.getUsername().toUpperCase());
-			rs = stmt.executeQuery();
-			boolean found = false;
-			if(rs.next()) {
-				//assuming all user Ids are unique/no duplicate
-				found = true;
-				logger.debug ("getQueue user found: " + user.getUsername());
-			}
-			if(found) {
-				user.setElectronicMailAddress(rs.getString("electronic_mail_address"));
-				user.setAccountStatus(rs.getString("ACCOUNT_STATUS"));
-				user.setExpiryDate(rs.getDate("EXPIRY_DATE"));
-				user.setLockDate(rs.getDate("LOCK_DATE"));
-				user.setPasswordChangedDate(rs.getDate("PTIME"));
-				user.setDateModified(rs.getDate("DATE_MODIFIED"));
-				user.setAttemptedCount(rs.getInt("ATTEMPTED_COUNT"));
-				user.setProcessingType(rs.getString("PROCESSING_TYPE"));
-				user.setDeliveryStatus(rs.getString("DELIVERY_STATUS"));
-				ret = user;
-			}
-		} catch (Exception ex) {
-			logger.debug(ex.getMessage());
-		} finally {
-            if (rs != null) { try { rs.close(); } catch (SQLException e) { logger.error(e.getMessage()); } }
-            if (stmt != null) {  try { stmt.close(); } catch (SQLException e) { logger.error(e.getMessage()); } }
-        	if (conn != null) { try { conn.close(); conn = null; } catch (SQLException e) { logger.error(e.getMessage()); } }
-		}
-		
-		return ret;
-	}
-
-	/**
-	 * Method to insert/update a password expiration queue specific to a user.
-	 * @param user the user (to be) notified
-	 * @throws Exception 
-	 */
-	public void updateQueue(User user) throws Exception {
-
-		logger.info("updateQueue entered");
-		if(user == null || user.getUsername() == null) {
-			throw new Exception("User or user's name can not be NULL or empty.");
-		}
-		
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		String value = null;
-		List arr = new ArrayList();
-		try {
-	        if(conn == null) {
-	        	DataSource ds = ConnectionUtil.getDS(DAO._jndiSystem);
-		        logger.debug("got DataSource for " + _jndiSystem);
-		        conn = ds.getConnection();
-	        }
-	        logger.debug("connected");
-
-			stmt = conn.prepareStatement("select * from SBREXT.PASSWORD_NOTIFICATION where UA_NAME = ?");
-			stmt.setString(1, user.getUsername().toUpperCase());
-			rs = stmt.executeQuery();
-			boolean found = false;
-			if(rs.next()) {
-				//assuming all user Ids are unique/no duplicate
-				found = true;
-				logger.debug ("updateQueue user found: " + user.getUsername());
-			}
-			if(!found) {
-				logger.debug ("updateQueue user not found: " + user.getUsername());
-				stmt = conn.prepareStatement("insert into SBREXT.PASSWORD_NOTIFICATION (ua_name, date_modified, attempted_count, processing_type, delivery_status) values(?,?,?,?,?)");
-				logger.debug ("updateQueue new queue");
-			} else {
-				logger.debug ("updateQueue user found: " + user.getUsername());
-				stmt = conn.prepareStatement("update SBREXT.PASSWORD_NOTIFICATION set ua_name = ?, date_modified = ?, attempted_count = ?, processing_type = ?, delivery_status = ?");
-				logger.debug ("updateQueue existing queue");
-			}
-			stmt.setString(1, user.getUsername().toUpperCase());
-			stmt.setDate(2, user.getDateModified());
-			stmt.setInt(3, user.getAttemptedCount());
-			stmt.setString(4, user.getProcessingType());
-			stmt.setString(5, user.getDeliveryStatus());
-			stmt.executeUpdate();
-		} catch (Exception ex) {
-			logger.debug(ex.getMessage());
-		} finally {
-            if (rs != null) { try { rs.close(); } catch (SQLException e) { logger.error(e.getMessage()); } }
-            if (stmt != null) {  try { stmt.close(); } catch (SQLException e) { logger.error(e.getMessage()); } }
-        	if (conn != null) { try { conn.close(); conn = null; } catch (SQLException e) { logger.error(e.getMessage()); } }
-		}
-	}
-
-	/**
-	 * Method to delete a queue specific to a user.
-	 * @param user the user (to be) notified
-	 * @throws Exception 
-	 */
-	public void removeQueue(User user) throws Exception {
-
-		logger.info("removeQueue entered");
-		if(user == null || user.getUsername() == null) {
-			throw new Exception("User or user's name can not be NULL or empty.");
-		}
-		
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		String value = null;
-		List arr = new ArrayList();
-		try {
-	        if(conn == null) {				
-	        	DataSource ds = ConnectionUtil.getDS(DAO._jndiSystem);
-		        logger.debug("got DataSource for " + _jndiSystem);
-	        	
-		        conn = ds.getConnection();
-	        }
-	        logger.debug("connected");
-
-			stmt = conn.prepareStatement("delete from SBREXT.PASSWORD_NOTIFICATION where UA_NAME = ?");
-			stmt.setString(1, user.getUsername().toUpperCase());
-			rs = stmt.executeQuery();
-			logger.debug ("removeQueue user : " + user.getUsername() + " queue removed");
-			stmt = conn.prepareStatement("alter user " + user.getUsername().toUpperCase() + " profile \"cadsr_user\"");
-			stmt.executeUpdate();
-			logger.debug ("updateQueue cadsr_user profile reapplied");
-		} catch (Exception ex) {
-			logger.debug(ex.getMessage());
-		} finally {
-            if (rs != null) { try { rs.close(); } catch (SQLException e) { logger.error(e.getMessage()); } }
-            if (stmt != null) {  try { stmt.close(); } catch (SQLException e) { logger.error(e.getMessage()); } }
-        	if (conn != null) { try { conn.close(); conn = null; } catch (SQLException e) { logger.error(e.getMessage()); } }
-		}
 	}
 	
 }
