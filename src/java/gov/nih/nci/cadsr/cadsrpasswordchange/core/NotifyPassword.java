d7817788f3011a17a8a93df14d2bbd032cfa2442
James Tan <tanj3@mail.nih.gov>
2012-09-27 17:57:43 +0000

    Remove OracleDatasource and use direct jdbc.
    SVN-Revision: 408

--- src/java/gov/nih/nci/cadsr/cadsrpasswordchange/core/NotifyPassword.java
+++ src/java/gov/nih/nci/cadsr/cadsrpasswordchange/core/NotifyPassword.java
@@ -4,6 +4,7 @@ import gov.nih.nci.cadsr.cadsrpasswordchange.domain.User;
 
 import java.io.FileInputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -55,24 +56,24 @@ public class NotifyPassword {
         {
             OracleDataSource ods = new OracleDataSource();
 
-            String parts[] = _dsurl.split("[:]");
-            ods.setDriverType("thin");
-            _logger.info("NotifyPassword v1.0 build 16.1");
-//            _logger.debug("NotifyPassword:open before [3]=" + parts[3] + " [4]=" + parts[4] + " [5]=" + parts[5]);
-//            parts[3] = parts[3].substring(1, parts[3].length());
-//            _logger.info("NotifyPassword:open after [3]=" + parts[3] + " [4]=" + parts[4] + " [5]=" + parts[5]);
-//            ods.setServerName(parts[3]);
-//            ods.setPortNumber(Integer.parseInt(parts[4]));
-//            ods.setServiceName(parts[5]);
-            //http://docs.oracle.com/cd/B19306_01/java.102/b14355/jdbcthin.htm
-//            String connString="jdbc:oracle:thin:@prodHost:1521:ORCL";
-            String connString=_dsurl;
-            ods.setURL(connString);
-            ods.setUser(_user);
-            ods.setPassword(_pswd);
-            _logger.info("NotifyPassword:open _dsurl[" + _dsurl + "] via _user["+ _user + "]");
-//            Connection conn = ods.getConnection();
-            _conn = ods.getConnection(_user, _pswd);
+//            String parts[] = _dsurl.split("[:]");
+//            ods.setDriverType("thin");
+            _logger.info("NotifyPassword v1.0 build 16.2");
+//            String connString=_dsurl;
+//            ods.setURL(connString);
+//            ods.setUser(_user);
+//            ods.setPassword(_pswd);
+//            _logger.info("NotifyPassword:open _dsurl[" + _dsurl + "] via _user["+ _user + "]");
+//            _conn = ods.getConnection(_user, _pswd);
+
+          String jdbcurl = PropertyHelper.getDatabaseURL();
+          _logger.debug("got connection using direct jdbc url [" + jdbcurl + "]");
+          Properties info = new Properties();
+          info.put( "user", PropertyHelper.getDatabaseUserID() );
+          _logger.debug("with user id [" + PropertyHelper.getDatabaseUserID() + "]");
+          info.put( "password", PropertyHelper.getDatabasePassword() );
+          Connection conn = DriverManager.getConnection(jdbcurl, info);
+            
             _logger.info("connected to the database");
             _conn.setAutoCommit(true);
             return 0;
