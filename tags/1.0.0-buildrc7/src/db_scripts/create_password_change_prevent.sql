7e460563a4f86eb4297125be2c8b881478fbe89c
James Tan <tanj3@mail.nih.gov>
2012-10-19 20:41:35 +0000

    .
    SVN-Revision: 539

---
+++ tags/1.0.0-buildrc7/src/db_scripts/create_password_change_prevent.sql
@@ -0,0 +1,20 @@
+-- run as sys
+
+CREATE OR REPLACE FUNCTION password_change_prevent (username_in VARCHAR2
+                                        , password VARCHAR2
+                                        , old_password varchar2)
+    RETURN boolean
+    IS
+
+    BEGIN
+
+        if (UPPER(username_in) in ('CADSRPASSWORDCHANGE', 'CDEBROWSER', 'CDECURATE', 'FORMBUILDER', 'FREESTYLESEARCH', 'SENTINEL',
+                   'SBR', 'SBREXT', 'CADSRADMIN', 'CADSR_API', 'UMLLDR', 'THE_SIW', 'BLKLDR', 'GUEST', 'SITESCOPE')) then
+            raise_application_error(-20010, 'Account is a special account, password cannot be changed.');
+        end if;
+
+
+        RETURN(true);
+
+    END;
+/
