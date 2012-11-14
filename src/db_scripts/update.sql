21147c121af732b04f4587c5d518617e233083b8
James Tan <tanj3@mail.nih.gov>
2012-11-14 14:40:30 +0000

    SVN-Revision: 577

---
+++ src/db_scripts/update.sql
@@ -0,0 +1,3 @@
+update sbrext.tool_options_view_ext set value = 'Your password of the account ${userid} is about to expire on ${expiryDate}.  To change your password, you can either login to the Password Change Station by visiting https://${webHost}, contact the NCI Helpdesk at ncicb@pop.nci.nih.gov or toll free phone number: 888-478-4423.'
+where Tool_name = 'PasswordChangeStation' and Property = 'EMAIL.INTRO'
+/
