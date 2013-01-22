53ecad32faf42664c73ab3098b6d75da0b77f4f4
James Tan <tanj3@mail.nih.gov>
2013-01-22 13:35:29 +0000

    CADSRPASSW-98 caDSR Password Notification Email Subject Change Request.
    SVN-Revision: 615

--- src/db_scripts/update-notification-email-details.sql
+++ src/db_scripts/update-notification-email-details.sql
@@ -3,5 +3,8 @@
 update sbrext.tool_options_view_ext set value = 'Your password for the caDSR account ${userid} is about to expire on ${expiryDate}.  To change your password, you can either login to the Password Change Station by visiting https://${webHost}, contact the NCI Helpdesk at ncicb@pop.nci.nih.gov or toll free phone number: 888-478-4423.'
 where Tool_name = 'PasswordChangeStation' and Property = 'EMAIL.INTRO'
 /
+update sbrext.tool_options_view_ext set value = 'CADSR: Password Expiration'
+where Tool_name = 'PasswordChangeStation' and Property = 'EMAIL.SUBJECT'
+/
 commit
 /
