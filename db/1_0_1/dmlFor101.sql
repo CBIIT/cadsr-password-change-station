c8e46872260df527d2aacd6998e7d595252f4e29
purnimac <purnimac@yahoo.com>
2016-01-15 11:07:44 -0500

    DB instructions for 1.0.1

--- db/1_0_1/dmlFor101.sql
+++ db/1_0_1/dmlFor101.sql
@@ -1,3 +1,5 @@
+--Execute the following 2 update statements in the SBREXT schema.
+
 update sbrext.tool_options_view_ext set value='Your password for the caDSR account ${userid} is about to expire on ${expiryDate}.  To change your password, you can either login to the Password Change Station by visiting https://${webHost} or contact the NCI Helpdesk at ncicbiit@mail.nih.gov.' where Tool_name = 'PasswordChangeStation' and Property = 'EMAIL.INTRO';
 
 update sbrext.tool_options_view_ext set value='http://cbiit.nci.nih.gov/ncip/biomedical-informatics-resources/interoperability-and-semantics/metadata-and-models' where Tool_name = 'PasswordChangeStation' and Property = 'LOGO.ROOT';
