4783628fe170219a2bf2d7f3ca418d881a4ad4fd
purnimac <purnimac@yahoo.com>
2016-01-15 10:46:55 -0500

    Update the helpdesk email information. 1

---
+++ db/1_0_1/dmlFor101.sql
@@ -0,0 +1,3 @@
+update sbrext.tool_options_view_ext set value='Your password for the caDSR account ${userid} is about to expire on ${expiryDate}.  To change your password, you can either login to the Password Change Station by visiting https://${webHost} or contact the NCI Helpdesk at ncicbiit@mail.nih.gov.' where Tool_name = 'PasswordChangeStation' and Property = 'EMAIL.INTRO';
+
+update sbrext.tool_options_view_ext set value='http://cbiit.nci.nih.gov/ncip/biomedical-informatics-resources/interoperability-and-semantics/metadata-and-models' where Tool_name = 'PasswordChangeStation' and Property = 'LOGO.ROOT';
