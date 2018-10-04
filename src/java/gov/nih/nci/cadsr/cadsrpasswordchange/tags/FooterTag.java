bfa79f2250e07c3c49572cbe0e44081a3d031b84
vikramsan <vikram.san@gmail.com>
2018-10-04 12:38:37 -0400

    App Support email change & app version

--- src/java/gov/nih/nci/cadsr/cadsrpasswordchange/tags/FooterTag.java
+++ src/java/gov/nih/nci/cadsr/cadsrpasswordchange/tags/FooterTag.java
@@ -44,7 +44,7 @@ public class FooterTag extends TagSupport {
 	    		   + "<tr><td class=\"ncifmenu\"><div aria-hidden=true><span style=\"color: #dddddd\">" + version + "." + buildTime + "</span></div></td></tr>"
 	    		   + "<tr><td class=\"nciftrtable\">"
 	    		   + "<div aria-hidden=true role=presentation tabindex=-1>"
-	    		   + "<a href=\"mailto:ncicbiit@mail.nih.gov?subject=caDSR%20Password%20Change%20Station\"><img border=\"0\" src=\"/cadsrpasswordchange/images/email_icon.gif\" alt=\"Email NCI Help Desk\" title=\"Email NCI Help Desk\"></a>"
+	    		   + "<a href=\"mailto:NCIAppSupport@nih.gov?subject=caDSR%20Password%20Change%20Station\"><img border=\"0\" src=\"/cadsrpasswordchange/images/email_icon.gif\" alt=\"Email NCI Help Desk\" title=\"Email NCI Help Desk\"></a>"
 	    		   + "<a target=\"_blank\" href=\"http://www.cancer.gov/\"><img border=\"0\" src=\"/cadsrpasswordchange/images/footer_nci.gif\" alt=\"National Cancer Institute Logo\" title=\"National Cancer Institute\"></a>"
 	    		   + "<a target=\"_blank\" href=\"http://www.dhhs.gov/\"><img border=\"0\" src=\"/cadsrpasswordchange/images/footer_hhs.gif\" alt=\"Department of Health and Human Services Logo\" title=\"Department of Health and Human Services\"></a>"
 	    		   + "<a target=\"_blank\" href=\"http://www.nih.gov/\"><img border=\"0\" src=\"/cadsrpasswordchange/images/footer_nih.gif\" alt=\"National Institutes of Health Logo\" title=\"National Institutes of Health\"></a>"
